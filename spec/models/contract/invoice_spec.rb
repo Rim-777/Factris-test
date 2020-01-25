0043d802f3a73fd007728bf3b91926d7b9bc36ea
Timo Moss <javatimomoss@gmail.com>
2020-01-25 18:12:46 +0200

    added specs for models

--- spec/models/contract/invoice_spec.rb
+++ spec/models/contract/invoice_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Contract::Invoice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should belong_to(:contract).inverse_of(:invoices).touch(true)}
 end
--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
-RSpec.describe Contract::Create do
 
+RSpec.describe Contract, type: :model do
+  it {should have_many(:invoices).class_name('Contract::Invoice').inverse_of(:contract).dependent(:destroy)}
 end
